937cd0c3e9691567338a969a5fc9bc74e433cad9
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-12-05 20:24:57 -0800

    read me

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,124 +2,121 @@
 
 
 // module 4 lesson 4 tell you how to clikc the buttton and go to the next page.
-var timerClock = document.getElementById('timer');
-var questions = document.querySelector(".questions");
+var timerClock = document.getElementById("timer");
+
+var currentQuestion = document.querySelector("current-question");
 
 
-function displayMessage() {
+var quizQuestions = [{
+   question: "1. var a = 11;" + 
+   "b = a - 10;" +
+   "console.log(b);" +
+   "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 3
+}, {
+   question: "1. var a = 11;" + 
+   "a - 10;" +
+   "console.log(a);" +
+   "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 1
+}, {
+   question: "3. var a = 11;" + 
+   "a = a - 10;" +
+   "console.log(a);" +
+   "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 3
+}, {
+   question: "4. var a = 11;" + 
+   "a = c - 10;" +
+   "console.log(a);" +
+   "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 0
+}, {
+   question: "5. var a = 11;" + 
+   "a = c - 10;" +
+   "console.log(c);" +
+   "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 5
+},{
+  question: "6. var a = 11;" + 
+  "10 = a - a;" +
+  "console.log(a);" +
+  "What will be logged in the console?;",
+   choices: ["a;", "11;", "10;", "1;", "0;", "error or undefined;"],
+   correctAnswer: 5
+}];
+
+function quiz(quizQuestions) {
+   this.score = 0;
+   this.quesstions = questions;
+   this.questionIndex = 0;
+}
+
+
+
+
+
+
+var quiz = Quiz(quizQuestions);
+
    var clock = 50;
-   var msgInterval = setInterval(function() {
+   var timer = setInterval(function() {
+      
      if (clock >= 0) {      
        timerClock.textContent = clock;
          clock--;
+      }
+      else {
+     
+        clearInterval(timer);
+        console.log('yes2"')
+      }
+     }, 1000);
+
+     function showQuestions(questions, currentQuestion){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
+   
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+         
+         // first reset the list of answers
+         answers = [];
+   
+         // for each available answer to this question...
+         for(number in questions[i].answers){
+console.log(questions.length)
+answers.push(
+   '<label>'
+      + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+      + letter + ': '
+      + questions[i].answers[letter]
+   + '</label>'
+);
+}
+
+// add this question and its answers to the output
+output.push(
+'<div class="question">' + questions[i].question + '</div>'
++ '<div class="answers">' + answers.join('') + '</div>'
+);
+}
+
+// finally combine our output list into one string of html and put it on the page
+currentQuestion.innerHTML = output.join('');
+}
 
 
-// var qOne = function(){
 
-//     var a = 11;
-//    b = a - 10;
-
-//    console.log(b)
-
-//    What will be logged in the console?
-//    li & button = 11
-//    li & button = 10
-//     li & button = 1 
-//    li & button = 0
-//    li & button = error or undefined
-
-// }
+   
 
 
-// var qTwo = function(){
-
-//     var a = 11;
-//    a - 10;
-
-//    console.log(a)
-
-//    What will be logged in the console?
-//    li & button = a
-//    li & button = 11 // answer
-//    li & button = 10
-//     li & button = 1 
-//    li & button = 0
-//    li & button = error or undefined
-
-// }
-
-// var qThree = function(){
-
-//     var a = 11;
-//    a = a - 10;
-
-//    console.log(a)
-
-//    What will be logged in the console?
-//    li & button = 11 
-//    li & button = 10
-//     li & button = 1 // answer
-//    li & button = 0
-//    li & button = error or undefined
-
-// }
-
-// var qFour = function(){
-
-//     var a = 11;
-//    a = c - 10;
-
-//    console.log(a)
-
-//    What will be logged in the console?
-//    li & button = 11 // answer
-//    li & button = 10
-//    li & button = 1 
-//    li & button = 0 
-//    li & button = error or undefined
-
-// }
-
-// var qFive = function(){
-
-//     var a = 11;
-//    a = c - 10;
-
-//    console.log(c)
-
-//    What will be logged in the console?
-//    li & button = 11 // answer
-//    li & button = 10
-//     li & button = 1 
-//    li & button = 0
-//    li & button = error or undefined
-
-// }
-
-// var qSix= function(){
-
-//     var a = 11;
-//    10 = a -=14 a;
-
-//    console.log(a)
-
-//    What will be logged in the console?
-//    li & button = 11 // answer
-//    li & button = 10
-//    li & button = 1 
-//    li & button = 0
-//    li & button = error or undefined
-
-// }
-
-     }
- else {
-
-   clearInterval(msgInterval);
-   console.log('yes2"')
- }
-}, 1000);
-};
 
 
-displayMessage();
+         showQuestions(questions, currentQuestion);
