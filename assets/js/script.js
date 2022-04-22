e5c6fc6a42658445302efc945293338066f500ee
JosephJamesCoop <cooper_3869@live.com>
2022-04-21 22:41:07 -0700

    btn working

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,60 +68,23 @@ const quizQuestions = [{
    correctAnswer: 5
 }];
 
-
-
 const totalTime = 160;
-
 const newQuiz = () => {
     questionIndex = 0;
     totalTime = 160;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
-
-    const startTimer = setInterval(function() {
-        totalTime--;
-        timeLeft.textContent = totalTime;
-        if(totalTime <= 0) {
-            clearInterval(startTimer);
-            if (questionIndex < questions.length - 1) {
-                endGame();
-            }
-        }
-    },1000);
-};
-startQuizBtn.addEventListener("click", newQuiz);
-choiceA.addEventListener("click", chooseA);
-choiceB.addEventListener("click", chooseB);
-choiceC.addEventListener("click", chooseC);
-choiceD.addEventListener("click", chooseD);
-choiceE.addEventListener("click", chooseE);
-
-const endGame = () => {
-
-
-   finalScore.textContent = correctAns;
-}
-
-
-const totalTime = 151;
-const newQuiz = () => {
-    questionIndex = 0;
-    totalTime = 150;
-    timeLeft.textContent = totalTime;
-    initialInput.textContent = "";
-
     startDiv.style.display = "none";
     questionDiv.style.display = "block";
     timer.style.display = "block";
     timesUp.style.display = "none";
-
     const startTimer = setInterval(function() {
         totalTime--;
         timeLeft.textContent = totalTime;
         if(totalTime <= 0) {
             clearInterval(startTimer);
             if (questionIndex < questions.length - 1) {
-                endGame();
+                gameOver();
             }
         }
     },1000);
@@ -136,12 +99,9 @@ const checkAnswer = (answer) => {
     answerCheck.style.display = "block";
 
     if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
-     
         correctAns++;
-  
         answerCheck.textContent = "Correct!";
     } else {
-
         totalTime -= 10;
         timeLeft.textContent = totalTime;
         answerCheck.textContent = "Wrong! The correct answer is: " + questions[questionIndex].answer;
@@ -151,29 +111,47 @@ const checkAnswer = (answer) => {
     if (questionIndex < questions.length) {
         nextQuestion();
     } else {
-
-        endGame();
+        gameOver();
     }
 }
 
-const chooseA = () => { checkAnswer(0); };
-const chooseB = () => { checkAnswer(1); };
-const chooseC = () => { checkAnswer(2); };
-const chooseD = () => { checkAnswer(3); };
+const nextQuestion = () => {
+   questionTitle.textContent = questions[questionIndex].question;
+   choiceA.textContent = questions[questionIndex].choices[0];
+   choiceB.textContent = questions[questionIndex].choices[1];
+   choiceC.textContent = questions[questionIndex].choices[2];
+   choiceD.textContent = questions[questionIndex].choices[3];
+   choiceD.textContent = questions[questionIndex].choices[4];
+}
 
-const endGame = () => {
+const chooseA = () => { checkAnswer(0); }
+const chooseB = () => { checkAnswer(1); }
+const chooseC = () => { checkAnswer(2); }
+const chooseD = () => { checkAnswer(3); }
+const chooseE = () => { checkAnswer(4); }
+
+startQuizBtn.addEventListener("click", newQuiz);
+choiceA.addEventListener("click", chooseA);
+choiceB.addEventListener("click", chooseB);
+choiceC.addEventListener("click", chooseC);
+choiceD.addEventListener("click", chooseD);
+choiceE.addEventListener("click", chooseE);
+
+const gameOver = () => {
     summary.style.display = "block";
     questionDiv.style.display = "none";
     startDiv.style.display = "none";
     timer.style.display = "none";
     timesUp.style.display = "block";
-
     finalScore.textContent = correctAns;
+}
+
+const showQuiz = () => {
+   nextQuestion();
 }
 
 const storeHighScores = (event) => {
     event.preventDefault();
-
     if (initialInput.value === "") {
         alert("Please enter your initials!");
         return;
@@ -199,8 +177,43 @@ const storeHighScores = (event) => {
         score: finalScore.textContent
     };
     scoresArray.push(userScore);
-
     const scoresArrayString = JSON.stringify(scoresArray);
     window.localStorage.setItem("high scores", scoresArrayString);
-   
+
+    showHighScores();
 }
+
+const i = 0;
+const showHighScores = () => {
+
+    startDiv.style.display = "none";
+    timer.style.display = "none";
+    questionDiv.style.display = "none";
+    timesUp.style.display = "none";
+    summary.style.display = "none";
+    highScoreSection.style.display = "block";
+
+    const savedHighScores = localStorage.getItem("high scores");
+    if (savedHighScores === null) {
+        return;
+    }
+    const storedHighScores = JSON.parse(savedHighScores);
+    for (; i < storedHighScores.length; i++) {
+        const eachNewHighScore = document.createElement("p");
+        eachNewHighScore.innerHTML = storedHighScores[i].initials + ": " + storedHighScores[i].score;
+        listOfHighScores.appendChild(eachNewHighScore);
+    }
+}
+
+submitBtn.addEventListener("click", function(event){ 
+    storeHighScores(event);
+});
+
+viewHighScore.addEventListener("click", function(event) { 
+    showHighScores(event);
+});
+
+goBackBtn.addEventListener("click", function() {
+    startDiv.style.display = "block";
+    highScoreSection.style.display = "none";
+});
